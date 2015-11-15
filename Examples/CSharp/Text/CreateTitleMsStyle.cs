37b04b4863f7ae651c31ad7357c809fd13a43c41
Saqib Masood <saqib@masood.pk>
2015-11-15 20:59:08 +0500

    Add C# and VisualBasic examples for 2.0.0

---
+++ Examples/CSharp/Text/CreateTitleMsStyle.cs
@@ -0,0 +1,48 @@
+﻿//////////////////////////////////////////////////////////////////////////
+// Copyright 2001-2015 Aspose Pty Ltd. All Rights Reserved.
+//
+// This file is part of Aspose.Note. The source code in this file
+// is only intended as a supplement to the documentation, and is provided
+// "as is", without warranty of any kind, either expressed or implied.
+//////////////////////////////////////////////////////////////////////////
+using System;
+using System.Globalization;
+using Aspose.Note;
+
+namespace Aspose.Note.Examples.Text
+{
+    class CreateTitleMsStyle
+    {
+        static void Main()
+        {
+            string dataDir = Aspose.Note.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+            string outputPath = dataDir + "Output.one";
+
+            var doc = new Document();
+            var page = new Page(doc);
+
+            page.Title = new Title(doc)
+            {
+                TitleText = new RichText(doc)
+                {
+                    Text = "Title text.",
+                    DefaultStyle = TextStyle.DefaultMsOneNoteTitleTextStyle
+                },
+                TitleDate = new RichText(doc)
+                {
+                    Text = new DateTime(2011, 11, 11).ToString("D", CultureInfo.InvariantCulture),
+                    DefaultStyle = TextStyle.DefaultMsOneNoteTitleDateStyle
+                },
+                TitleTime = new RichText(doc)
+                {
+                    Text = "12:34",
+                    DefaultStyle = TextStyle.DefaultMsOneNoteTitleTimeStyle
+                }
+            };
+
+            doc.AppendChild(page);
+
+            doc.Save(outputPath);
+        }
+    }
+}
---
+++ Examples/VisualBasic/Text/CreateTitleMsStyle.vb
@@ -0,0 +1,41 @@
+﻿'////////////////////////////////////////////////////////////////////////
+' Copyright 2001-2015 Aspose Pty Ltd. All Rights Reserved.
+'
+' This file is part of Aspose.Note. The source code in this file
+' is only intended as a supplement to the documentation, and is provided
+' "as is", without warranty of any kind, either expressed or implied.
+'////////////////////////////////////////////////////////////////////////
+
+Imports System
+Imports System.Globalization
+Imports Aspose.Note
+
+Namespace Aspose.Note.Examples.Text
+    Public Class CreateTitleMsStyle
+        Public Shared Sub Main(ByVal args() As String)
+            Dim dataDir As String = Aspose.Note.Examples.Utils.GetDataDir(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType)
+            Dim outputPath As String = dataDir & "Output.one"
+
+            Dim doc = New Document()
+            Dim page = New Page(doc)
+            page.Title = New Title(doc) With { _
+                .TitleText = New RichText(doc) With { _
+                    .Text = "Title text.", _
+                    .DefaultStyle = TextStyle.DefaultMsOneNoteTitleTextStyle _
+                }, _
+                .TitleDate = New RichText(doc) With { _
+                    .Text = New DateTime(2011, 11, 11).ToString("D", CultureInfo.InvariantCulture), _
+                    .DefaultStyle = TextStyle.DefaultMsOneNoteTitleDateStyle _
+                }, _
+                .TitleTime = New RichText(doc) With { _
+                    .Text = "12:34", _
+                    .DefaultStyle = TextStyle.DefaultMsOneNoteTitleTimeStyle _
+                } _
+            }
+
+            doc.AppendChild(Page)
+
+            doc.Save(outputPath)
+        End Sub
+    End Class
+End Namespace
