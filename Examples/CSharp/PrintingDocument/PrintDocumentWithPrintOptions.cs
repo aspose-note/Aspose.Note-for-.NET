a1a4cc308c13a8e7923d15aaf1846d4394d2db5e
Aspose.Email Product Family <kashif.iqbal@aspose.com>
2017-04-03 20:58:43 +0500

    Added following topics for Aspose.Note for .NET 17.3.0: - PrintDocument - PrintDocumentWithPrintingOptions

---
+++ Examples/CSharp/PrintingDocument/PrintDocumentWithPrintOptions.cs
@@ -0,0 +1,34 @@
+﻿using Aspose.Note.Saving;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+
+namespace Aspose.Note.Examples.CSharp.PrintingDocument
+{
+    class PrintDocumentWithPrintOptions
+    {
+        // ExStart:PrintDocumentWithPrintOptions
+        public static void Run()
+        {
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_LoadingAndSaving();
+
+            var document = new Aspose.Note.Document(dataDir + "Aspose.one");
+
+            var printerSettings = new PrinterSettings() { FromPage = 0, ToPage = 10 };
+            printerSettings.DefaultPageSettings.Landscape = true;
+            printerSettings.DefaultPageSettings.Margins = new System.Drawing.Printing.Margins(50, 50, 150, 50);
+
+            document.Print(new PrintOptions()
+            {
+                PrinterSettings = printerSettings,
+                Resolution = 1200,
+                PageSplittingAlgorithm = new KeepSolidObjectsAlgorithm(),
+                DocumentName = "Test.one"
+            });
+        }
+        // ExStart:PrintDocumentWithPrintOptions
+    }
+}
