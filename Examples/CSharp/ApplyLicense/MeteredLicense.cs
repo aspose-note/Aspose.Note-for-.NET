73d025bfb2363b13328f6031a8057c773e1cf5b5
Alexei Soloview <alexei.soloview@aspose.com>
2022-04-08 16:54:54 +0300

    New sample for Metered license usage

---
+++ Examples/CSharp/ApplyLicense/MeteredLicense.cs
@@ -0,0 +1,41 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="MeteredLicense.cs" company="Aspose Pty Ltd">
+//    Copyright (c) 2002-2022 Aspose Pty Ltd. All Rights Reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace Aspose.Note.Examples.CSharp.ApplyLicense
+{
+    using System;
+    using System.IO;
+
+    class MeteredLicense
+    {
+        public static void Run()
+        {
+            // ExStart:MeteredLicense
+            // ExFor:Metered
+            // ExFor:Metered.SetMeteredKey
+            // ExSummary:Shows how to set metered license.
+
+            Metered metered = new Metered();
+            metered.SetMeteredKey("MyPublicKey", "MyPrivateKey");
+
+            Console.WriteLine($"Credit before operation: {Metered.GetConsumptionCredit():F2}");
+            Console.WriteLine($"Consumption quantity before operation: {Metered.GetConsumptionQuantity():F2}");
+
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Pages();
+
+            // Load OneNote document and get first child           
+            Document document = new Document(Path.Combine(dataDir, "Aspose.one"));
+            
+            document.Save(Path.Combine(dataDir, "MeteredLicense.pdf"));
+
+            Console.WriteLine($"Credit before operation: {Metered.GetConsumptionCredit():F2}");
+            Console.WriteLine($"Consumption quantity before operation: {Metered.GetConsumptionQuantity():F2}");
+
+            // ExEnd:MeteredLicense
+        }
+    }
+}
